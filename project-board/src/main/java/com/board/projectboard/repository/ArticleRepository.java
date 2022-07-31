a2838d4090de1a5085038f521e944add219a2d35
cohouseol <cohouseol@gmail.com>
2022-07-31 17:18:44 +0900

    Revert "#9 - repository interface 작성"
    This reverts commit c6a2c75339df78c0378421f51548e321719c6013.

--- project-board/src/main/java/com/board/projectboard/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.board.projectboard.repository;
-
-import com.board.projectboard.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
