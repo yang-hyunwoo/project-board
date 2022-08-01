a5d66b66e14f1d423e71624d2a0f3b278086cf1b
cohouseol <cohouseol@gmail.com>
2022-08-01 17:49:00 +0900

    #18 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서  부가적인 옵션이 들어갈 수 있음을 확인했기 때문에
    적절히 테스트 메소드 수정

--- project-board/src/test/java/com/board/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/board/projectboard/controller/ArticleControllerTest.java
@@ -13,7 +13,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
-@WebMvcTest(ArticleComment.class)
+@WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
     private final MockMvc mvc;
@@ -21,7 +21,6 @@ class ArticleControllerTest {
     ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -30,10 +29,11 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("articles/detail"))
-                .andExpect(model().attributeExists("articles"))
-                .andExpect(model().attributeExists("articleComments"));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+
+//                .andExpect(model().attributeExists("articleComments"));
     }
 
     @Disabled("구현 중")
