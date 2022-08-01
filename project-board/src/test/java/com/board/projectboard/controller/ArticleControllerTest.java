95edb18355fbcf1c7992cd7e3fad35420207bbf5
cohouseol <cohouseol@gmail.com>
2022-08-01 18:02:38 +0900

    #18 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 a5d66b6 에서 확인했기 때문에,
    나머지 뷰도 동일하게 작동할 것으로 예상하고
    미리 테스트 메소드 업데이트

--- project-board/src/test/java/com/board/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/com/board/projectboard/controller/ArticleControllerTest.java
@@ -45,7 +45,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles"));
     }
 
@@ -58,7 +58,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
 
@@ -71,7 +71,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
