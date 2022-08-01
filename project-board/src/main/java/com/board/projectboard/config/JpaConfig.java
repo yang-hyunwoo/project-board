6d256a9bae344348c0f37eaea6133f901724414b
cohouseol <cohouseol@gmail.com>
2022-08-01 13:48:12 +0900

    modify
    어제자 커밋 이상하여 수정하여 다시 반영.

---
+++ project-board/src/main/java/com/board/projectboard/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.board.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("gusdn");              //TODO: 스프링 시큐리티로 인증 기능을 붙이게 될때 , 수정
+    }
+}
